21883304f532875f80f85ba8b7798a84fce74207
Risalagustian5 <muhammadrisalagustian5@gmail.com>
2026-06-06 11:24:31 +0700

    nambah routes dll

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Menampilkan halaman login (welcome.blade.php) saat pertama dibuka
 Route::get('/', function () {
+    return view('welcome'); 
+});
+
+Route::get('/login', function () {
     return view('welcome');
+});
+
+// Menampilkan halaman register di dalam folder auth
+Route::get('/register', function () {
+    return view('auth.register'); 
+});
+
+// Menampilkan halaman dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
